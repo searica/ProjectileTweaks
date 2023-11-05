71f65b09ec8985d3ed2e0eee042f946f4acd2cdd
searica <143636061+searica@users.noreply.github.com>
2023-11-05 09:11:43 -0800

    Default settings

--- Configs/Config.cs
+++ Configs/Config.cs
@@ -13,8 +13,6 @@ namespace ProjectileTweaks.Configs
 {
     internal class Config
     {
-        private static BaseUnityPlugin configurationManager;
-
         private static readonly string ConfigFileName = ProjectileTweaks.PluginGUID + ".cfg";
 
         private static readonly string ConfigFileFullPath = string.Concat(
@@ -40,7 +38,6 @@ namespace ProjectileTweaks.Configs
         internal static bool IsVerbosityMedium => Verbosity.Value >= LoggerLevel.Medium;
         internal static bool IsVerbosityHigh => Verbosity.Value >= LoggerLevel.High;
 
-        private const string MainSectionName = "\u200BGlobal";
         private const string BowSection = "BowTweaks";
         private const string XbowSection = "CrossbowTweaks";
         private const string StaffSection = "StaffTweaks";
@@ -66,6 +63,7 @@ namespace ProjectileTweaks.Configs
         internal static ConfigEntry<LoggerLevel> Verbosity { get; private set; }
 
         private static readonly AcceptableValueList<bool> AcceptableBoolValuesList = new(new bool[] { false, true });
+        private const char ZWS = '\u200B';
 
         internal static ConfigEntry<T> BindConfig<T>(
             string section,
@@ -114,26 +112,38 @@ namespace ProjectileTweaks.Configs
             configFile.SaveOnConfigSet = value;
         }
 
+        /// <summary>
+        ///     Sets SaveOnConfigSet to false and returns
+        ///     the value prior to calling this method.
+        /// </summary>
+        /// <returns></returns>
+        private static bool DisableSaveOnConfigSet()
+        {
+            var val = configFile.SaveOnConfigSet;
+            configFile.SaveOnConfigSet = false;
+            return val;
+        }
+
         internal static void SetUpConfig()
         {
             // Bows
             BowSpreadMult = BindConfig<float>(
                 BowSection,
-                "SpreadMultiplier",
+                new string(ZWS, 3) + "SpreadMultiplier",
                 1,
                 "Multiplies the min and max projectile spread, so if you set it to zero your arrows will have zero spread.",
                 new AcceptableValueRange<float>(0, 2)
             );
             BowVelocityMult = BindConfig<float>(
                 BowSection,
-                "VelocityMultiplier",
+                new string(ZWS, 2) + "VelocityMultiplier",
                 1,
                 "Multiplies velocity of arrows.",
                 new AcceptableValueRange<float>(0.1f, 2)
             );
             BowLaunchAngle = BindConfig<float>(
                 BowSection,
-                "LaunchAngle",
+                ZWS + "LaunchAngle",
                 -1,
                 "Changes the launch angle for arrows. Vanilla default for bows is 0. Negative values angle upwards, and positive values angle downwards.",
                 new AcceptableValueRange<float>(-5, 5)
@@ -141,36 +151,36 @@ namespace ProjectileTweaks.Configs
             BowHorizontalOffset = BindConfig<float>(
                 BowSection,
                 "HorizontalOffset",
-                0,
-                "Offsets the location that arrows are spawned before firing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
+                0.2f,
+                "Offsets the location that arrows are launched from when firing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
                 new AcceptableValueRange<float>(-0.5f, 0.5f)
             );
             BowVerticalOffset = BindConfig<float>(
                 BowSection,
                 "VerticalOffset",
-                0,
-                "Offsets the location that arrows are spawned before firing them. Positive shifts it upwards. Negative shifts it downwards.",
+                0.2f,
+                "Offsets the location that arrows are launched from when firing them. Positive shifts it upwards. Negative shifts it downwards.",
                 new AcceptableValueRange<float>(-0.5f, 0.5f)
             );
 
             // Crossbows
             XbowSpreadMult = BindConfig<float>(
                 XbowSection,
-                "SpreadMultiplier",
+                new string(ZWS, 3) + "SpreadMultiplier",
                 1,
                 "Multiplies the min and max projectile spread, so if you set it to zero your bolts will have zero spread.",
                 new AcceptableValueRange<float>(0, 2)
             );
             XbowVelocityMult = BindConfig<float>(
                 XbowSection,
-                "VelocityMultiplier",
+                new string(ZWS, 2) + "VelocityMultiplier",
                 1,
                 "Multiplies velocity of bolts.",
                 new AcceptableValueRange<float>(0.1f, 2)
             );
             XbowLaunchAngle = BindConfig<float>(
                 XbowSection,
-                "LaunchAngle",
+                ZWS + "LaunchAngle",
                 -1,
                 "Changes the launch angle for bolts. Vanilla default for crossbows is -1. Negative values angle upwards, and positive values angle downwards.",
                 new AcceptableValueRange<float>(-5, 5)
@@ -179,28 +189,28 @@ namespace ProjectileTweaks.Configs
                 XbowSection,
                 "HorizontalOffset",
                 0,
-                "Offsets the location that bolts are spawned before firing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
+                "Offsets the location that bolts are launched from when firing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
                 new AcceptableValueRange<float>(-0.5f, 0.5f)
             );
             XbowVerticalOffset = BindConfig<float>(
                 XbowSection,
                 "VerticalOffset",
                 0,
-                "Offsets the location that bolts are spawned before firing them. Positive shifts it upwards. Negative shifts it downwards.",
+                "Offsets the location that bolts are launched from when firing them. Positive shifts it upwards. Negative shifts it downwards.",
                 new AcceptableValueRange<float>(-0.5f, 0.5f)
             );
 
             // Staffs
             StaffSpreadMult = BindConfig<float>(
                 StaffSection,
-                "SpreadMultiplier",
+                new string(ZWS, 3) + "SpreadMultiplier",
                 1,
                 "Multiplies the min and max projectile spread, so if you set it to zero there will be zero spread.",
                 new AcceptableValueRange<float>(0, 2)
             );
             StaffVelocityMult = BindConfig<float>(
                 StaffSection,
-                "VelocityMultiplier",
+                new string(ZWS, 2) + "VelocityMultiplier",
                 1,
                 "Multiplies velocity of projectiles.",
                 new AcceptableValueRange<float>(0.1f, 2)
@@ -216,14 +226,14 @@ namespace ProjectileTweaks.Configs
                 StaffSection,
                 "HorizontalOffset",
                 0,
-                "Offsets the location that projectiles are spawned before firing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
+                "Offsets the location that projectiles are launched from when firing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
                 new AcceptableValueRange<float>(-0.5f, 0.5f)
             );
             StaffVerticalOffset = BindConfig<float>(
                 StaffSection,
                 "VerticalOffset",
-                0,
-                "Offsets the location that projectiles are spawned before firing them. Positive shifts it upwards. Negative shifts it downwards.",
+                0.3f,
+                "Offsets the location that projectiles are launched from when firing them. Positive shifts it upwards. Negative shifts it downwards.",
                 new AcceptableValueRange<float>(-0.5f, 0.5f)
             );
 
@@ -249,68 +259,17 @@ namespace ProjectileTweaks.Configs
                 Log.LogInfo("Reloading config file");
 
                 // turn off saving on config entry set
-                var saveOnConfigSet = configFile.SaveOnConfigSet;
-                configFile.SaveOnConfigSet = false;
+                var saveOnConfigSet = DisableSaveOnConfigSet();
 
                 configFile.Reload();
 
-                // reset config saving state
+                // reset config saving state to previous state
                 configFile.SaveOnConfigSet = saveOnConfigSet;
             }
             catch
             {
                 Log.LogError($"There was an issue loading your {ConfigFileName}");
                 Log.LogError("Please check your config entries for spelling and format!");
-            }
-            // Do whatever else I might need to
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        internal static void CheckForConfigManager()
-        {
-            if (GUIManager.IsHeadless())
-            {
-                return;
-            }
-
-            if (
-                Chainloader.PluginInfos.TryGetValue(
-                    "com.bepis.bepinex.configurationmanager",
-                    out PluginInfo configManagerInfo
-                )
-                && configManagerInfo.Instance
-            )
-            {
-                configurationManager = configManagerInfo.Instance;
-                Log.LogDebug("Configuration manager found, hooking DisplayingWindowChanged");
-
-                EventInfo eventinfo = configurationManager.GetType()
-                    .GetEvent("DisplayingWindowChanged");
-
-                if (eventinfo != null)
-                {
-                    Action<object, object> local = new(OnConfigManagerDisplayingWindowChanged);
-                    Delegate converted = Delegate.CreateDelegate(
-                        eventinfo.EventHandlerType,
-                        local.Target,
-                        local.Method
-                    );
-                    eventinfo.AddEventHandler(configurationManager, converted);
-                }
-            }
-        }
-
-        private static void OnConfigManagerDisplayingWindowChanged(object sender, object e)
-        {
-            //Jotunn.Logger.LogDebug("OnConfigManagerDisplayingWindowChanged received.");
-            PropertyInfo pi = configurationManager.GetType().GetProperty("DisplayingWindow");
-            bool cmActive = (bool)pi.GetValue(configurationManager, null);
-
-            if (!cmActive)
-            {
-                // do whatever I need to do when the cfg file reloads
             }
         }
     }
