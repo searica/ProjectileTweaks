ba841bc151404aa2a652af10ab6cefb23d33dc7f
searica <143636061+searica@users.noreply.github.com>
2023-11-22 15:24:04 -0800

    Tidying code style

--- Patches/AttackPatch.cs
+++ Patches/AttackPatch.cs
@@ -1,11 +1,11 @@
-﻿using HarmonyLib;
-using UnityEngine;
-using ProjectileTweaks.Configs;
+﻿using UnityEngine;
+using HarmonyLib;
+using BepInEx.Configuration;
 
 namespace ProjectileTweaks.Patches
 {
     [HarmonyPatch(typeof(Attack))]
-    internal class AttackPatch
+    internal static class AttackPatch
     {
         [HarmonyPostfix]
         [HarmonyBefore("blacks7ar.BowPlugin")]
@@ -19,160 +19,117 @@ namespace ProjectileTweaks.Patches
             Skills.SkillType skillType = weapon.m_shared.m_skillType;
             if (skillType == Skills.SkillType.Bows)
             {
-                // Velocity
-                Config.BowVelocityMult.SettingChanged += delegate
-                {
-                    __instance.m_projectileVel *= Config.BowVelocityMult.Value;
-                    __instance.m_projectileVelMin *= Config.BowVelocityMult.Value;
-                };
-                __instance.m_projectileVel *= Config.BowVelocityMult.Value;
-                __instance.m_projectileVelMin *= Config.BowVelocityMult.Value;
-
-                // Spread
-                Config.BowSpreadMult.SettingChanged += delegate
-                {
-                    __instance.m_projectileAccuracy *= Config.BowSpreadMult.Value;
-                    __instance.m_projectileAccuracyMin *= Config.BowSpreadMult.Value;
-                };
-                __instance.m_projectileAccuracy *= Config.BowSpreadMult.Value;
-                __instance.m_projectileAccuracyMin *= Config.BowSpreadMult.Value;
-
-                // Launch angle
-                Config.BowLaunchAngle.SettingChanged += delegate
-                {
-                    __instance.m_launchAngle = Config.BowLaunchAngle.Value;
-                };
-                __instance.m_launchAngle = Config.BowLaunchAngle.Value;
+                SetVelocity(__instance, ProjectileTweaks.BowVelocityMult);
+                SetSpread(__instance, ProjectileTweaks.BowSpreadMult);
+                SetLaunchAngle(__instance, ProjectileTweaks.BowLaunchAngle);
             }
             else if (skillType == Skills.SkillType.Crossbows)
             {
-                // Velocity
-                Config.XbowVelocityMult.SettingChanged += delegate
-                {
-                    __instance.m_projectileVel *= Config.XbowVelocityMult.Value;
-                    __instance.m_projectileVelMin *= Config.XbowVelocityMult.Value;
-                };
-                __instance.m_projectileVel *= Config.XbowVelocityMult.Value;
-                __instance.m_projectileVelMin *= Config.XbowVelocityMult.Value;
-
-                // Spread
-                Config.XbowSpreadMult.SettingChanged += delegate
-                {
-                    __instance.m_projectileAccuracy *= Config.XbowSpreadMult.Value;
-                    __instance.m_projectileAccuracyMin *= Config.XbowSpreadMult.Value;
-                };
-                __instance.m_projectileAccuracy *= Config.XbowSpreadMult.Value;
-                __instance.m_projectileAccuracyMin *= Config.XbowSpreadMult.Value;
-
-                // Launch angle
-                Config.XbowLaunchAngle.SettingChanged += delegate
-                {
-                    __instance.m_launchAngle = Config.XbowLaunchAngle.Value;
-                };
-                __instance.m_launchAngle = Config.XbowLaunchAngle.Value;
+                SetVelocity(__instance, ProjectileTweaks.XbowVelocityMult);
+                SetSpread(__instance, ProjectileTweaks.XbowSpreadMult);
+                SetLaunchAngle(__instance, ProjectileTweaks.XbowLaunchAngle);
             }
             else if (skillType == Skills.SkillType.Spears)
             {
-                // Velocity
-                Config.SpearVelocityMult.SettingChanged += delegate
-                {
-                    __instance.m_projectileVel *= Config.SpearVelocityMult.Value;
-                    __instance.m_projectileVelMin *= Config.SpearVelocityMult.Value;
-                };
-                __instance.m_projectileVel *= Config.SpearVelocityMult.Value;
-                __instance.m_projectileVelMin *= Config.SpearVelocityMult.Value;
-
-                // Spread
-                Config.SpearSpreadMult.SettingChanged += delegate
-                {
-                    __instance.m_projectileAccuracy *= Config.SpearSpreadMult.Value;
-                    __instance.m_projectileAccuracyMin *= Config.SpearSpreadMult.Value;
-                };
-                __instance.m_projectileAccuracy *= Config.SpearSpreadMult.Value;
-                __instance.m_projectileAccuracyMin *= Config.SpearSpreadMult.Value;
-
-                // Launch angle
-                Config.SpearLaunchAngle.SettingChanged += delegate
-                {
-                    __instance.m_launchAngle = Config.SpearLaunchAngle.Value;
-                };
-                __instance.m_launchAngle = Config.SpearLaunchAngle.Value;
+                SetVelocity(__instance, ProjectileTweaks.SpearVelocityMult);
+                SetSpread(__instance, ProjectileTweaks.SpearSpreadMult);
+                SetLaunchAngle(__instance, ProjectileTweaks.SpearLaunchAngle);
             }
             else if (skillType == Skills.SkillType.ElementalMagic)
             {
-                // Velocity
-                Config.StaffVelocityMult.SettingChanged += delegate
-                {
-                    __instance.m_projectileVel *= Config.StaffVelocityMult.Value;
-                    __instance.m_projectileVelMin *= Config.StaffVelocityMult.Value;
-                };
-                __instance.m_projectileVel *= Config.StaffVelocityMult.Value;
-                __instance.m_projectileVelMin *= Config.StaffVelocityMult.Value;
-
-                // Spread
-                Config.StaffSpreadMult.SettingChanged += delegate
-                {
-                    __instance.m_projectileAccuracy *= Config.StaffSpreadMult.Value;
-                    __instance.m_projectileAccuracyMin *= Config.StaffSpreadMult.Value;
-                };
-                __instance.m_projectileAccuracy *= Config.StaffSpreadMult.Value;
-                __instance.m_projectileAccuracyMin *= Config.StaffSpreadMult.Value;
+                SetVelocity(__instance, ProjectileTweaks.StaffVelocityMult);
+                SetSpread(__instance, ProjectileTweaks.StaffSpreadMult);
             }
         }
 
-        // for unity
-        // private static readonly Vector3 forwardVector = new Vector3(0f, 0f, 1f);
+        private static void SetVelocity(Attack attack, ConfigEntry<float> velocityMultiplier)
+        {
+            velocityMultiplier.SettingChanged += (sender, e) =>
+            {
+                attack.m_projectileVel *= velocityMultiplier.Value;
+                attack.m_projectileVelMin *= velocityMultiplier.Value;
+            };
+            attack.m_projectileVel *= velocityMultiplier.Value;
+            attack.m_projectileVelMin *= velocityMultiplier.Value;
+        }
+
+        private static void SetSpread(Attack attack, ConfigEntry<float> spreadConfig)
+        {
+            spreadConfig.SettingChanged += (sender, e) =>
+            {
+                attack.m_projectileAccuracy *= spreadConfig.Value;
+                attack.m_projectileAccuracyMin *= spreadConfig.Value;
+            };
+            attack.m_projectileAccuracy *= spreadConfig.Value;
+            attack.m_projectileAccuracyMin *= spreadConfig.Value;
+        }
+
+        private static void SetLaunchAngle(Attack attack, ConfigEntry<float> angleConfig)
+        {
+            angleConfig.SettingChanged += (sender, e) =>
+            {
+                attack.m_launchAngle *= angleConfig.Value;
+            };
+            attack.m_launchAngle *= angleConfig.Value;
+        }
 
         [HarmonyPostfix]
         [HarmonyPatch(nameof(Attack.GetProjectileSpawnPoint))]
-        internal static void GetProjectileSpawnPointPostfix(Attack __instance, ref Vector3 spawnPoint, ref Vector3 aimDir)
+        private static void GetProjectileSpawnPointPostfix(Attack __instance, ref Vector3 spawnPoint, ref Vector3 aimDir)
         {
             var weapon = GetPlayerWeapon(__instance);
             if (weapon == null) { return; }
 
             // I think that I just want to use the aimDir vector to rotate the offsets
             // and then change the spawn point based on the config
-
             Skills.SkillType skillType = weapon.m_shared.m_skillType;
             if (skillType == Skills.SkillType.Bows)
             {
-                Log.LogInfo($"SpawnPoint {spawnPoint}");
-                var horizontalOffset = new Vector3(aimDir.z, 0, -aimDir.x).normalized;
-                horizontalOffset *= Config.BowHorizontalOffset.Value;
-                spawnPoint += horizontalOffset;
-                spawnPoint += new Vector3(0.0f, Config.BowVerticalOffset.Value, 0.0f);
-                Log.LogInfo($"SpawnPoint {spawnPoint}");
+                ApplyOffset(
+                    ref spawnPoint,
+                    ref aimDir,
+                    ProjectileTweaks.BowVerticalOffset.Value,
+                    ProjectileTweaks.BowHorizontalOffset.Value);
             }
             else if (skillType == Skills.SkillType.Crossbows)
             {
-                var horizontalOffset = new Vector3(aimDir.z, 0, -aimDir.x).normalized;
-                horizontalOffset *= Config.XbowHorizontalOffset.Value;
-                spawnPoint += horizontalOffset;
-                spawnPoint += new Vector3(0.0f, Config.XbowVerticalOffset.Value, 0.0f);
+                ApplyOffset(
+                    ref spawnPoint,
+                    ref aimDir,
+                    ProjectileTweaks.XbowVerticalOffset.Value,
+                    ProjectileTweaks.XbowHorizontalOffset.Value);
             }
             else if (skillType == Skills.SkillType.Spears)
             {
-                var horizontalOffset = new Vector3(aimDir.z, 0, -aimDir.x).normalized;
-                horizontalOffset *= Config.SpearHorizontalOffset.Value;
-                spawnPoint += horizontalOffset;
-                spawnPoint += new Vector3(0.0f, Config.SpearVerticalOffset.Value, 0.0f);
+                ApplyOffset(
+                    ref spawnPoint,
+                    ref aimDir,
+                    ProjectileTweaks.SpearVerticalOffset.Value,
+                    ProjectileTweaks.SpearHorizontalOffset.Value);
             }
             else if (skillType == Skills.SkillType.ElementalMagic)
             {
-                var horizontalOffset = new Vector3(aimDir.z, 0, -aimDir.x).normalized;
-                horizontalOffset *= Config.StaffHorizontalOffset.Value;
-                spawnPoint += horizontalOffset;
-                spawnPoint += new Vector3(0.0f, Config.StaffVerticalOffset.Value, 0.0f);
+                ApplyOffset(
+                    ref spawnPoint,
+                    ref aimDir,
+                    ProjectileTweaks.StaffVerticalOffset.Value,
+                    ProjectileTweaks.StaffHorizontalOffset.Value);
             }
+        }
+
+        private static void ApplyOffset(ref Vector3 spawnPoint, ref Vector3 aimDir, float vertOffset, float horzOffset)
+        {
+            var horizontalOffset = new Vector3(aimDir.z, 0, -aimDir.x).normalized;
+            horizontalOffset *= horzOffset;
+            spawnPoint += horizontalOffset;
+            spawnPoint += new Vector3(0.0f, vertOffset, 0.0f);
         }
 
         private static ItemDrop.ItemData GetPlayerWeapon(Attack attack)
         {
-            if (
-                attack == null ||
+            if (attack == null ||
                 attack.m_character is not Player player ||
-                !(player.m_name == "Human" || player == Player.m_localPlayer)
-            )
+                !(player.m_name == "Human" || player == Player.m_localPlayer))
             {
                 return null;
             }
--- ProjectileTweaks.cs
+++ ProjectileTweaks.cs
@@ -1,12 +1,12 @@
 ﻿// Ignore Spelling: Plugin Jotunn
-
+using System.Reflection;
 using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
 using Jotunn.Utils;
-using Jotunn.Entities;
-using Jotunn.Managers;
-using System.Reflection;
+using UnityEngine;
+using ProjectileTweaks.Configs;
 
 namespace ProjectileTweaks
 {
@@ -15,57 +15,295 @@ namespace ProjectileTweaks
     [NetworkCompatibility(CompatibilityLevel.VersionCheckOnly, VersionStrictness.Patch)]
     internal class ProjectileTweaks : BaseUnityPlugin
     {
-        internal const string Author = "Searica";
+        public const string Author = "Searica";
         public const string PluginName = "ProjectileTweaks";
         public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
         public const string PluginVersion = "1.0.3";
 
         // Use this class to add your own localization to the game
         // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
-        public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
+        //private static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
 
+
+        private const string BowSection = "BowTweaks";
+        private const string XbowSection = "CrossbowTweaks";
+        private const string SpearSection = "SpearTweaks";
+        private const string StaffSection = "StaffTweaks";
+
+        internal static ConfigEntry<float> BowSpreadMult { get; set; }
+        internal static ConfigEntry<float> BowVelocityMult { get; set; }
+        internal static ConfigEntry<float> BowLaunchAngle { get; set; }
+        internal static ConfigEntry<float> BowVerticalOffset { get; set; }
+        internal static ConfigEntry<float> BowHorizontalOffset { get; set; }
+
+        internal static ConfigEntry<float> XbowSpreadMult { get; set; }
+        internal static ConfigEntry<float> XbowVelocityMult { get; set; }
+        internal static ConfigEntry<float> XbowLaunchAngle { get; set; }
+        internal static ConfigEntry<float> XbowVerticalOffset { get; set; }
+        internal static ConfigEntry<float> XbowHorizontalOffset { get; set; }
+
+        internal static ConfigEntry<float> SpearSpreadMult { get; set; }
+        internal static ConfigEntry<float> SpearVelocityMult { get; set; }
+        internal static ConfigEntry<float> SpearLaunchAngle { get; set; }
+        internal static ConfigEntry<float> SpearVerticalOffset { get; set; }
+        internal static ConfigEntry<float> SpearHorizontalOffset { get; set; }
+
+        internal static ConfigEntry<float> StaffSpreadMult { get; set; }
+        internal static ConfigEntry<float> StaffVelocityMult { get; set; }
+        internal static ConfigEntry<float> StaffLaunchAngle { get; set; }
+        internal static ConfigEntry<float> StaffVerticalOffset { get; set; }
+        internal static ConfigEntry<float> StaffHorizontalOffset { get; set; }
+
+
+        /// <summary>
+        ///     Called by Unity
+        /// </summary>
         public void Awake()
         {
-            Log.Init(Logger);
+            Log.Init(this.Logger);
 
-            Configs.Config.Init(Config);
-            Configs.Config.SetUpConfig();
+            ConfigManager.Init(PluginGUID, Config, false);
+            Initialize();
+            ConfigManager.Save();
+
 
             Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
 
             Game.isModded = true;
 
-            Configs.Config.SetupWatcher();
+            ConfigManager.SetupWatcher();
         }
 
+        /// <summary>
+        ///     Called by Unity
+        /// </summary>
         public void OnDestroy()
         {
-            Configs.Config.Save();
+            ConfigManager.Save();
+        }
+
+        private void Initialize()
+        {
+            // Bows
+            BowSpreadMult = ConfigManager.BindConfig(
+                BowSection,
+                ConfigManager.SetStringPriority("SpreadMultiplier", 3),
+                1f,
+                "Multiplies the min and max projectile spread, so if you set it to zero your arrows will have zero spread.",
+                new AcceptableValueRange<float>(0f, 2f));
+            BowVelocityMult = ConfigManager.BindConfig(
+                BowSection,
+                ConfigManager.SetStringPriority("VelocityMultiplier", 2),
+                1f,
+                "Multiplies velocity of arrows.",
+                new AcceptableValueRange<float>(0.1f, 2f));
+            BowLaunchAngle = ConfigManager.BindConfig(
+                BowSection,
+                ConfigManager.SetStringPriority("LaunchAngle", 1),
+                -1f,
+                "Changes the launch angle for arrows. Vanilla default for bows is 0. Negative values angle upwards, and positive values angle downwards.",
+                new AcceptableValueRange<float>(-5f, 5f));
+            BowHorizontalOffset = ConfigManager.BindConfig<float>(
+                BowSection,
+                "HorizontalOffset",
+                0.2f,
+                "Offsets the location that arrows are launched from when firing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
+                new AcceptableValueRange<float>(-0.5f, 0.5f));
+            BowVerticalOffset = ConfigManager.BindConfig<float>(
+                BowSection,
+                "VerticalOffset",
+                0.2f,
+                "Offsets the location that arrows are launched from when firing them. Positive shifts it upwards. Negative shifts it downwards.",
+                new AcceptableValueRange<float>(-0.5f, 0.5f));
+
+            // Crossbows
+            XbowSpreadMult = ConfigManager.BindConfig(
+                XbowSection,
+                ConfigManager.SetStringPriority("SpreadMultiplier", 3),
+                1f,
+                "Multiplies the min and max projectile spread, so if you set it to zero your bolts will have zero spread.",
+                new AcceptableValueRange<float>(0f, 2f));
+            XbowVelocityMult = ConfigManager.BindConfig(
+                XbowSection,
+                ConfigManager.SetStringPriority("VelocityMultiplier", 2),
+                1f,
+                "Multiplies velocity of bolts.",
+                new AcceptableValueRange<float>(0.1f, 2f));
+            XbowLaunchAngle = ConfigManager.BindConfig(
+                XbowSection,
+                ConfigManager.SetStringPriority("LaunchAngle", 1),
+                -1f,
+                "Changes the launch angle for bolts. Vanilla default for crossbows is -1. Negative values angle upwards, and positive values angle downwards.",
+                new AcceptableValueRange<float>(-5f, 5f));
+            XbowHorizontalOffset = ConfigManager.BindConfig(
+                XbowSection,
+                "HorizontalOffset",
+                0f,
+                "Offsets the location that bolts are launched from when firing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
+                new AcceptableValueRange<float>(-0.5f, 0.5f));
+            XbowVerticalOffset = ConfigManager.BindConfig(
+                XbowSection,
+                "VerticalOffset",
+                0f,
+                "Offsets the location that bolts are launched from when firing them. Positive shifts it upwards. Negative shifts it downwards.",
+                new AcceptableValueRange<float>(-0.5f, 0.5f));
+
+            // Spears
+            SpearSpreadMult = ConfigManager.BindConfig(
+                SpearSection,
+                ConfigManager.SetStringPriority("SpreadMultiplier", 3),
+                1f,
+                "Multiplies the min and max projectile spread, so if you set it to zero your spear throws will have zero spread.",
+                new AcceptableValueRange<float>(0f, 2f));
+            SpearVelocityMult = ConfigManager.BindConfig(
+                SpearSection,
+                ConfigManager.SetStringPriority("VelocityMultiplier", 2),
+                1f,
+                "Multiplies velocity of thrown spears.",
+                new AcceptableValueRange<float>(0.1f, 2f));
+            SpearLaunchAngle = ConfigManager.BindConfig(
+                SpearSection,
+                "LaunchAngle",
+                -1f,
+                "Changes the launch angle for thrown spears. Vanilla default for spears is 0. Negative values angle upwards, and positive values angle downwards.",
+                new AcceptableValueRange<float>(-5f, 5f));
+            SpearHorizontalOffset = ConfigManager.BindConfig(
+                SpearSection,
+                "HorizontalOffset",
+                0.1f,
+                "Offsets the location that thrown spears are launched from when throwing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
+                new AcceptableValueRange<float>(-0.5f, 0.5f));
+            SpearVerticalOffset = ConfigManager.BindConfig(
+                SpearSection,
+                "VerticalOffset",
+                0.5f,
+                "Offsets the location that thrown spears are launched from when throwing them. Positive shifts it upwards. Negative shifts it downwards.",
+                new AcceptableValueRange<float>(-0.5f, 0.5f));
+
+            // Staffs
+            StaffSpreadMult = ConfigManager.BindConfig(
+                StaffSection,
+                ConfigManager.SetStringPriority("SpreadMultiplier", 3),
+                1f,
+                "Multiplies the min and max projectile spread, so if you set it to zero there will be zero spread.",
+                new AcceptableValueRange<float>(0f, 2f));
+            StaffVelocityMult = ConfigManager.BindConfig(
+                StaffSection,
+                ConfigManager.SetStringPriority("VelocityMultiplier", 2),
+                1f,
+                "Multiplies velocity of projectiles.",
+                new AcceptableValueRange<float>(0.1f, 2f));
+            StaffHorizontalOffset = ConfigManager.BindConfig(
+                StaffSection,
+                "HorizontalOffset",
+                0f,
+                "Offsets the location that projectiles are launched from when firing them. Positive shifts it to your characters right. Negative shifts it to your characters left.",
+                new AcceptableValueRange<float>(-0.5f, 0.5f));
+            StaffVerticalOffset = ConfigManager.BindConfig(
+                StaffSection,
+                "VerticalOffset",
+                0.3f,
+                "Offsets the location that projectiles are launched from when firing them. Positive shifts it upwards. Negative shifts it downwards.",
+                new AcceptableValueRange<float>(-0.5f, 0.5f));
+        }
+    }
+}
+
+/// <summary>
+///     Log level to control output to BepInEx log
+/// </summary>
+internal enum LogLevel
+{
+    Low = 0,
+    Medium = 1,
+    High = 2,
+}
+
+/// <summary>
+///     Helper class for properly logging from static contexts.
+/// </summary>
+internal static class Log
+{
+    private const BindingFlags AllBindings =
+           BindingFlags.Public
+           | BindingFlags.NonPublic
+           | BindingFlags.Instance
+           | BindingFlags.Static
+           | BindingFlags.GetField
+           | BindingFlags.SetField
+           | BindingFlags.GetProperty
+           | BindingFlags.SetProperty;
+
+    #region Verbosity
+
+    internal static ConfigEntry<LogLevel> Verbosity { get; set; }
+    internal static LogLevel VerbosityLevel => Verbosity.Value;
+    internal static bool IsVerbosityLow => Verbosity.Value >= LogLevel.Low;
+    internal static bool IsVerbosityMedium => Verbosity.Value >= LogLevel.Medium;
+    internal static bool IsVerbosityHigh => Verbosity.Value >= LogLevel.High;
+
+    #endregion Verbosity
+
+    private static ManualLogSource logSource;
+
+    internal static void Init(ManualLogSource logSource)
+    {
+        Log.logSource = logSource;
+    }
+
+    internal static void LogDebug(object data) => logSource.LogDebug(data);
+
+    internal static void LogError(object data) => logSource.LogError(data);
+
+    internal static void LogFatal(object data) => logSource.LogFatal(data);
+
+    internal static void LogMessage(object data) => logSource.LogMessage(data);
+
+    internal static void LogWarning(object data) => logSource.LogWarning(data);
+
+    internal static void LogInfo(object data, LogLevel level = LogLevel.Low)
+    {
+        if (Verbosity is null || VerbosityLevel >= level)
+        {
+            logSource.LogInfo(data);
         }
     }
 
-    /// <summary>
-    /// Helper class for properly logging from static contexts.
-    /// </summary>
-    internal static class Log
+    internal static void LogGameObject(GameObject prefab, bool includeChildren = false)
     {
-        internal static ManualLogSource _logSource;
-
-        internal static void Init(ManualLogSource logSource)
+        LogInfo("***** " + prefab.name + " *****");
+        foreach (Component compo in prefab.GetComponents<Component>())
         {
-            _logSource = logSource;
+            LogComponent(compo);
         }
 
-        internal static void LogDebug(object data) => _logSource.LogDebug(data);
+        if (!includeChildren) { return; }
 
-        internal static void LogError(object data) => _logSource.LogError(data);
+        LogInfo("***** " + prefab.name + " (children) *****");
+        foreach (Transform child in prefab.transform)
+        {
+            LogInfo($" - {child.gameObject.name}");
+            foreach (Component compo in child.gameObject.GetComponents<Component>())
+            {
+                LogComponent(compo);
+            }
+        }
+    }
 
-        internal static void LogFatal(object data) => _logSource.LogFatal(data);
+    internal static void LogComponent(Component compo)
+    {
+        LogInfo($"--- {compo.GetType().Name}: {compo.name} ---");
 
-        internal static void LogInfo(object data) => _logSource.LogInfo(data);
+        PropertyInfo[] properties = compo.GetType().GetProperties(AllBindings);
+        foreach (var property in properties)
+        {
+            LogInfo($" - {property.Name} = {property.GetValue(compo)}");
+        }
 
-        internal static void LogMessage(object data) => _logSource.LogMessage(data);
-
-        internal static void LogWarning(object data) => _logSource.LogWarning(data);
+        FieldInfo[] fields = compo.GetType().GetFields(AllBindings);
+        foreach (var field in fields)
+        {
+            LogInfo($" - {field.Name} = {field.GetValue(compo)}");
+        }
     }
 }
